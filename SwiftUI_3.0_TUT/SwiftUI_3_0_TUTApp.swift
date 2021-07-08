fb04260818d0093118007fead73e6803fe952385
SeonJungKim <ksj083895@gmail.com>
2021-07-08 14:53:46 +0900

    Initial Commit

---
+++ SwiftUI_3.0_TUT/SwiftUI_3_0_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3_0_TUTApp.swift
+//  SwiftUI_3.0_TUT
+//
+//  Created by Seogun Kim on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_3_0_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
